c29972ba45119f2d661a927cfcb678daafa104ab
Silvio_Martinico <96478908+SilvioM97@users.noreply.github.com>
2022-10-31 17:37:43 +0100

    Update squares_of_a_sorted_array.rs

--- squares_of_a_sorted_array.rs
+++ squares_of_a_sorted_array.rs
@@ -1,9 +1,9 @@
 //Linear solution O(n)
 
-//At the moment of submission (24/09/22):
+//At the moment of submission (31/10/22):
 /*
-Runtime: 5 ms, faster than 99.28% of Rust online submissions for Palindrome Number.
-Memory Usage: 2.3 MB, less than 59.71% of Rust online submissions for Palindrome Number.
+Runtime: 7 ms, faster than 98.20% of Rust online submissions for Squares of a Sorted Array.
+Memory Usage: 2.2 MB, less than 93.53% of Rust online submissions for Squares of a Sorted Array.
 */
 
 
@@ -13,16 +13,16 @@ impl Solution {
         let mut res: Vec<i32> = vec![0; n];
         
         if n == 1 {
-            res[0] = nums[0]*nums[0];
+            res[0] = nums[0].pow(2);
         }
         else if nums[0] >= 0 {
             for i in 0..n {
-                res[i] = (nums[i]*nums[i]);
+                res[i] = (nums[i].pow(2));
             }
         }
         else if nums[n-1] <= 0 {
             for i in 0..n {
-                res[n-i-1] = nums[i]*nums[i];
+                res[n-i-1] = nums[i].pow(2);
             }
         }
         else {
@@ -30,15 +30,15 @@ impl Solution {
             let mut d = n-1;
             while s < d {
                 if nums[s].abs() <= nums[d].abs() {
-                    res[d-s] = nums[d]*nums[d];
+                    res[d-s] = nums[d].pow(2);
                     d = d - 1;
                 } 
                 else {
-                    res[d-s] = nums[s]*nums[s];
+                    res[d-s] = nums[s].pow(2);
                     s = s + 1;
                 }
             }
-            res[0] = nums[s]*nums[s];
+            res[0] = nums[s].pow(2);
         }
         return res;
     }
